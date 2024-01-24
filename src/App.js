346ab3d1bcdc4f4d8d442bfcd39f59b5bd69e4cc
Akay82 <youremail@yourdomain.com>
2024-01-24 13:26:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css' //theme
+import 'primereact/resources/primereact.min.css' //core css
+import 'primeicons/primeicons.css'
+
+import {Editor} from 'primereact/editor'
+
+import { useState } from 'react'
 
 function App() {
+  const [value1, setValue1]=useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: "75px",  display:"flex", gap: "2rem", backgroundColor:"#A9A9A9"}}>
+
+    <div style={{width: "100%", border: "1px solid black"}}>
+      <Editor value={value1} onTextChange={(e)=> setValue1(e.htmlValue)} style={{height:"500px"}} />
+       </div>
     </div>
   );
 }
